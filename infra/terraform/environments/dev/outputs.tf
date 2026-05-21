f7170f09f37f085a81c88e69ac6d39384ad8291e
Julio Lira <yurgenlira@hotmail.com>
2026-05-21 13:27:41 -0600

    Fix/GitHub actions (#6)
    * fix(ci): update App Runner deployment to use service name instead of ARN

--- infra/terraform/environments/dev/outputs.tf
+++ infra/terraform/environments/dev/outputs.tf
@@ -6,6 +6,10 @@ output "app_runner_service_arn" {
   value = module.app_runner.service_arn
 }
 
+output "ecr_access_role_arn" {
+  value = module.app_runner.ecr_access_role_arn
+}
+
 output "s3_bucket_name" {
   value = module.s3.bucket_name
 }
